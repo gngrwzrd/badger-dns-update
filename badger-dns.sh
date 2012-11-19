43f1e687592152a98f1533df71ddffad2f1f2483
Aaron Smith <gngrwzrd@gmail.com>
2012-11-19 14:05:24 -0800

    Initial commit

---
+++ badger-dns.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+USER=$1
+PASS=$2
+DOMAIN=$3
+RECORDID=$4
+
+IPADDRESS=`curl -s http://checkip.dyndns.org | sed 's/[a-zA-Z/<> :]//g'`
+IPADDRESS=$(echo -n "${IPADDRESS//[[:space:]]/}")
+
+curl -X PUT -u "$USER:$PASS" \
+-H "Content-length: 0" \
+"https://api.badger.com/domains/$DOMAIN/records/$RECORDID?content=$IPADDRESS"
