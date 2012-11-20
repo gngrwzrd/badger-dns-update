c7b492009c849c2dcc0fe1f4c84a8cc3be00198a
Aaron Smith <gngrwzrd@gmail.com>
2012-11-20 12:20:00 -0800

    Added a log file

--- badger-dns.sh
+++ badger-dns.sh
@@ -3,10 +3,15 @@ USER=$1
 PASS=$2
 DOMAIN=$3
 RECORDID=$4
+LOGFILE=/tmp/badger-dns.log
+
+touch $LOGFILE
 
 IPADDRESS=`curl -s http://checkip.dyndns.org | sed 's/[a-zA-Z/<> :]//g'`
 IPADDRESS=$(echo -n "${IPADDRESS//[[:space:]]/}")
 
 curl -X PUT -u "$USER:$PASS" \
 -H "Content-length: 0" \
-"https://api.badger.com/domains/$DOMAIN/records/$RECORDID?content=$IPADDRESS"
+"https://api.badger.com/domains/$DOMAIN/records/$RECORDID?content=$IPADDRESS" \
+>> $LOGFILE
+echo "" >> $LOGFILE
